eccbc408396b2c3648186c1bda41b9c33f8e7b2b
Daniel Alejandro Lopez Naranjo <dalona126@hotmail.com>
2024-10-21 21:44:16 -0500

    Set Up Enviroment - Start App - Screens

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@
  * @format
  */
 
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -24,6 +25,9 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/home/Home';
+import { Booking } from './src/screens/booking/Booking';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -62,37 +66,16 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  
+const Stack = createNativeStackNavigator();
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Booking" component={Booking} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/booking/Booking.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Booking = () => {
+  return (
+    <View><Text>Booking</Text></View>
+  )
+}
